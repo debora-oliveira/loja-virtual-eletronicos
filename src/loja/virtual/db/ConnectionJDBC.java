62325f04f57c2eb7578c7439d4a14580ab2deca6
Debora Oliveira <debora.oliveira.dev@gmail.com>
2020-11-25 23:22:06 -0300

    classe inicial para testar banco

--- src/loja/virtual/db/ConnectionJDBC.java
+++ src/loja/virtual/db/ConnectionJDBC.java
@@ -7,5 +7,30 @@ import java.sql.SQLException;
 
 
 public class ConnectionJDBC {
+	//por enquanto apenas testando
+	public static void main(String[] args) {
+		// parametros para se conectar no banco
+		String driver = "mysql";
+		String dataBaseAddress = "localhost";
+		String dataBaseName = "lvEletronicos";
+		//criei um usuario a parte
+		String user = "ADMIN";
+		String password = "password";
+		//construindo string de conexao 
+		StringBuilder sb = new StringBuilder("jdbc:").append(driver)
+				.append("://").append(dataBaseAddress).append("/")
+				.append(dataBaseName);
 		
+		String connectionUrl = sb.toString();
+		
+		//criar conexao usando driverManager, passando como parametros a string de conexao, usuario e senha
+		try(Connection conn = DriverManager.getConnection(connectionUrl,user,password)){
+			System.out.println("SUCESSO ao se conectar ao banco MySQL!");
+		}catch (SQLException e) {
+			System.out.println("FALHA ao se conectar ao banco MYSQL");
+			e.printStackTrace();
+		}
+	}	
 }
+
+
